738e66d78873a93a954aa1ecb56ff004a8fb9b57
Rudy Crespo <crespor@montclair.edu>
2019-08-26 15:20:43 -0400

    created models

---
+++ app/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function order_details() {
+        return $this->hasMany('App\OrderDetail');
+    }
+}
