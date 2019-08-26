b67705e6162d88d84632d2feff372fe99642d3c6
Rudy Crespo <crespor@montclair.edu>
2019-08-26 16:27:29 -0400

    employee model

---
+++ app/Employee.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    public function employees() {
+        return $this->hasMany('App\Employee');
+    }
+}
