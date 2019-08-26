1b126079a56025d60066204c06d68cf87daa7e55
Rudy Crespo <crespor@montclair.edu>
2019-08-26 15:14:26 -0400

    order and order details tables

---
+++ database/migrations/2019_08_26_190626_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->date('order_date');
+            $table->date('ship_date')->nullable();
+            $table->boolean('is_shipped')->default(false);
+            $table->timestamps();
+            $table->foreign('customer_id')
+            ->references('id')
+            ->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2019_08_26_191145_create_order_details_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('order_id');
+            $table->smallInteger('quantity_ordered');
+            $table->decimal('price_each', 10, 2);
+            $table->timestamps();
+            
+            $table->foreign('customer_id')
+            ->references('id')
+            ->on('customers');
+
+            $table->foreign('order_id')
+            ->references('id')
+            ->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
