2855c59b6bbcd5b405c611dadf72d3be155a2292
Rudy Crespo <crespor@montclair.edu>
2019-08-28 00:06:30 -0400

    changed brand name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
         <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm fixed-top">
             <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Kimura BJJ Gear') }}
+                    {{ config('app.name', 'Fantastic Toys') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
