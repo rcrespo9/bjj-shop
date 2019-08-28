90658553b84cc8e36e8c4bfb5c0fba5348676b8b
Rudy Crespo <crespor@montclair.edu>
2019-08-28 10:25:14 -0400

    started on products api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Http\Resources\ProductResource;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductResource::collection(Product::all());
+    }
+}
